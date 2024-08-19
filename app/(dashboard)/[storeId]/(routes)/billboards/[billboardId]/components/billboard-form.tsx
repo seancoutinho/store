b84adf088cbb11d38836222cee73cd6601a00e95
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2024-08-19 16:13:08 +0200

    feat: completed billboard upload 4.22.37

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -23,6 +23,8 @@ import * as z from "zod";
 import { useParams, useRouter } from "next/navigation";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { useOrigin } from "@/hooks/use-origin";
+import { Input } from "@/components/ui/input";
+import ImageUpload from "@/components/ui/image-upload";
 
 const formSchema = z.object({
   label: z.string().min(1),
@@ -35,28 +37,41 @@ interface BillboardFormProps {
   initialData: Billboard | null;
 }
 
-export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => {
+export const BillboardForm: React.FC<BillboardFormProps> = ({
+  initialData,
+}) => {
   const params = useParams();
   const router = useRouter();
   const origin = useOrigin();
-  
+
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  const title = initialData ? "Edit Billboard" : "New Billboard";
+  const description = initialData ? "Edit a billboard" : "Add a new billboard";
+  const toastMessage = initialData
+    ? "Billboard updated successfully."
+    : "Billboard created successfully.";
+  const action = initialData ? "Save Changes" : "Create Billboard";
 
   const form = useForm<BillboardFormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: initialData || {
-      label: '',
-      imageUrl: '',
+      label: "",
+      imageUrl: "",
     },
   });
 
   const onSubmit = async (data: BillboardFormValues) => {
     try {
       setLoading(true);
-      await axios.patch(`/api/stores/${params.storeId}`, data);
+      if(initialData) {
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`, data);
+      } else {
+        await axios.post(`/api/${params.storeId}/billboards`, data);
+      }
       router.refresh();
-      toast.success("Store updated successfully");
+      toast.success(toastMessage);
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
@@ -67,12 +82,12 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/stores/${params.storeId}`);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
       router.refresh();
-      router.push('/')
-      toast.success("Store deleted successfully");
+      router.push("/");
+      toast.success('Billboard deleted successfully.');
     } catch (error) {
-      toast.error("Make sure to delete all products and categories first");
+      toast.error("Make sure to delete all categories using this billboard first");
     } finally {
       setLoading(false);
       setOpen(false);
@@ -88,29 +103,51 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
         loading={loading}
       />
       <div className="flex items-center justify-between">
-        <Heading title="Settings" description="Manage your store settings" />
-        <Button
-          disabled={loading}
-          variant="destructive"
-          size="sm"
-          onClick={() => setOpen(true)}>
-          <Trash size="sm" className="h-4 w-4" />
-        </Button>
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}>
+            <Trash size="sm" className="h-4 w-4" />
+          </Button>
+        )}
       </div>
       <Separator />
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full">
+          <FormField
+            control={form.control}
+            name="imageUrl"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background Image</FormLabel>
+                <FormControl>
+                  <ImageUpload 
+                    value={field.value ? [field.value] : []}
+                    disabled={loading}
+                    onChange={(url) => field.onChange(url)}
+                    onRemove={(url) => field.onChange('')}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="grid grid-cols-3 gap-8">
             <FormField
               control={form.control}
-              name="name"
+              name="label"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel>Name</FormLabel>
+                  <FormLabel>Label</FormLabel>
                   <FormControl>
-                    <input
+                    <Input
+                      disabled={loading}
+                      placeholder="Billboard Label"
                       {...field}
                       className="border rounded-md p-2 w-full"
                     />
@@ -121,11 +158,15 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({ initialData }) => 
             />
           </div>
           <Button disabled={loading} className="ml-auto" type="submit">
-            Save Changes
+            {action}
           </Button>
         </form>
         <Separator />
-        <ApiAlert title="NEXT_PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`}variant="public"/>
+        <ApiAlert
+          title="NEXT_PUBLIC_API_URL"
+          description={`${origin}/api/${params.storeId}`}
+          variant="public"
+        />
       </Form>
     </>
   );
---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,110 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse(JSON.stringify({ message: "Unauthenticated" }), {
+        status: 401,
+      });
+    }
+
+    if (!label) {
+      return new NextResponse(
+        JSON.stringify({ message: "Label is required" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!imageUrl) {
+      return new NextResponse(
+        JSON.stringify({ message: "Image URL is required" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    if (!params.storeId) {
+      return new NextResponse(
+        JSON.stringify({ message: "Store ID is required" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse(JSON.stringify({ message: "Unauthorized" }), {
+        status: 403,
+      });
+    }
+
+    const billboard = await prismadb.billboard.create({
+      data: {
+        label,
+        imageUrl,
+        storeId: params.storeId,
+      },
+    });
+
+    return new NextResponse(
+      JSON.stringify({ message: "Billboard successfully created", billboard }),
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log("[BILLBOARDS_POST]", error);
+    return new NextResponse(
+      JSON.stringify({ message: "Internal Server Error" }),
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    if (!params.storeId) {
+      return new NextResponse(
+        JSON.stringify({ message: "Store ID is required" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const billboards = await prismadb.billboard.findMany({
+      where: {
+        storeId: params.storeId,
+      },
+    });
+
+    return NextResponse.json(billboards);
+  } catch (error) {
+    console.log("[BILLBOARDS_GET]", error);
+    return new NextResponse(
+      JSON.stringify({ message: "Internal Server Error" }),
+      { status: 500 }
+    );
+  }
+}
